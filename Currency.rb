4ac8a9f0181453b662d0016667027b3674b08008
Alexandra Craig <alexandra.craig@gmail.com>
2016-09-19 16:39:30 -0400

    Currency class created. Created initialize, ==, !=, +, _, * methods

---
+++ Currency.rb
@@ -0,0 +1,52 @@
+class Currency
+  attr_accessor :amount, :code
+
+  def initialize(amount,code)
+    @amount = amount #@amount is the same thing as self.amount
+    @code = code
+  end
+
+  def ==(other)
+    @amount == other.amount && @code == other.code
+  end
+
+  def !=(other)
+    @amount == other.amount && @code == other.code
+  end
+
+  def +(other)
+    if @code == other.code
+      Currency.new(@amount + other.amount, @code)
+    else
+      raise DifferentCurrencyCodeError, "wrong currency types"
+    end
+  end
+
+  def -(other)
+    if @code == other.code
+      Currency.new(@amount - other.amount, @code)
+    else
+      raise DifferentCurrencyCodeError, "wrong currency types"
+    end
+  end
+
+  def *(other)
+    if @code == other.code
+      Currency.new(@amount * other.amount, @code)
+    else
+      raise DifferentCurrencyCodeError, "wrong currency types"
+    end
+  end
+end
+
+
+
+
+
+one_dollar = Currency.new(1, "USD")
+one_another_dollar = Currency.new(1, "USD")
+two_dollars = Currency.new(2, "USD")
+one_euro = Currency.new(1, "EUR")
+one_yuan = Currency.new(1, "CYN")
+three_dollars = one_dollar + two_dollars
+six_dollars = two_dollars * three_dollars
--- Currency_Converter.rb
+++ Currency_Converter.rb
@@ -3,4 +3,11 @@ class Currency
     @amount = amount
     @currency_code = currency_code
   end
+
+
+
 end
+
+dollars =  Currency.new(1, "usd")
+euro = Currency.new(1, "eur")
+yuan = Currency.new(1, "cyn")
