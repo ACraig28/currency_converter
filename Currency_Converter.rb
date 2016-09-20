986786b86b93c44f653052569f189802f10c4955
Alexandra Craig <alexandra.craig@gmail.com>
2016-09-20 09:20:14 -0400

    put back initialize method in class Currency Converter accidenty deleted

--- Currency_Converter.rb
+++ Currency_Converter.rb
@@ -1,6 +1,8 @@
 class CurrencyConverter
   attr_accessor :conversion_rate, :code,
+  def initialize
   @conversion_rates= {"USD": {"USD": 1, "EUR": 0.89423, "CYN": 6.67074}, "EUR": {"EURO": 1, "USD":1.11813, "CYN": 7.46003}, "CYN": {"CYN": 1, "USD": 0.149908, "EUR": 0.134040}}
+  end
   def convert_to_new_currency(current_currency, desired_currency)
     rate_to_be_used = @conversion_rates[current_currency.code][desired_currency]
     converted_currency = amount * rate_to_be_used
