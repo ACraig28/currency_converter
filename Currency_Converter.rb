4ca6e500625f6198cd05e6100514e21a6dcba106
Alexandra Craig <alexandra.craig@gmail.com>
2016-09-19 20:18:50 -0400

    sooooconfused

--- Currency_Converter.rb
+++ Currency_Converter.rb
@@ -1,8 +1,12 @@
 class CurrencyConverter
+    attr_accessor :conversion_rate, :code, :conversion_rate_hash
   def initialize(code, conversion_rate)
     @code = code
-    @conversion_rate
+    @conversion_rate = conversion_rate
+    conversion_rate_hash = {}
+    conversion_rate_hash[code] = conversion_rate
   end
+
 end
 
 dollars = CurrencyConverter.new("USD", 1)
